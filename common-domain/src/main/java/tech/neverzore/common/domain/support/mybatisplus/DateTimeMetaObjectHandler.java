7e04eb29164c2b1515cc28f2191e7ee85fa372d1
neverzore <zebon.zhou@gmail.com>
2020-09-29 22:45:21 +0800

    Add constructor for DateTimeMetaObjectHandler

--- common-domain/src/main/java/tech/neverzore/common/domain/support/mybatisplus/DateTimeMetaObjectHandler.java
+++ common-domain/src/main/java/tech/neverzore/common/domain/support/mybatisplus/DateTimeMetaObjectHandler.java
@@ -1,3 +1,5 @@
+
+
 package tech.neverzore.common.domain.support.mybatisplus;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
@@ -7,17 +9,29 @@ import tech.neverzore.common.domain.model.TraceableModel;
 import java.time.LocalDateTime;
 
 public class DateTimeMetaObjectHandler implements MetaObjectHandler {
+    private String cdt;
+    private String udt;
+
+    public DateTimeMetaObjectHandler() {
+        this("cdt", "udt");
+    }
+
+    public DateTimeMetaObjectHandler(String cdt, String udt) {
+        this.cdt = cdt;
+        this.udt = udt;
+    }
+
     @Override
     public void insertFill(MetaObject metaObject) {
         if (metaObject.getOriginalObject() instanceof TraceableModel) {
-            this.strictUpdateFill(metaObject, "cdt", () -> LocalDateTime.now(), LocalDateTime.class);
+            this.strictUpdateFill(metaObject, this.cdt, () -> LocalDateTime.now(), LocalDateTime.class);
         }
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         if (metaObject.getOriginalObject() instanceof TraceableModel) {
-            this.strictUpdateFill(metaObject, "udt", () -> LocalDateTime.now(), LocalDateTime.class);
+            this.strictUpdateFill(metaObject, this.udt, () -> LocalDateTime.now(), LocalDateTime.class);
         }
     }
 }
